a003ef116a01cc2f67e3bce0107d9a24b327117f
antwaunjohnson <antwaun.l.johnson@gmail.com>
2024-03-27 14:17:58 -0700

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    screens: {
+      xs: "375px",
+      sm: "480px",
+      md: "768px",
+      lg: "976",
+      xl: "1440px"
+    },
+    extend: {
+      colors: {
+        neonLime: 'hsl(75, 94%, 57%)',
+        white: 'hsl(0, 0%, 20%)',
+        darkGray: 'hsl(0, 0%, 12%)',
+        offBlack: 'hsl(0, 0%, 8%)'
+      },
+      fontFamily: {
+        sans: ["Inter", 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
