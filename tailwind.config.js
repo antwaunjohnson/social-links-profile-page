e3187e786cf549a3ba757dfbeb0ec4e75dfde15b
antwaunjohnson <antwaun.l.johnson@gmail.com>
2024-03-29 15:09:31 -0700

    recreate project from scratch

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
         neonLime: 'hsl(75, 94%, 57%)',
         white:'hsl(0, 0%, 100%)',
         grey: 'hsl(0, 0%, 20%)',
-        darkGray: 'hsl(0, 0%, 12%)',
+        darkGrey: 'hsl(0, 0%, 12%)',
         offBlack: 'hsl(0, 0%, 8%)'
       },
       fontFamily: {
